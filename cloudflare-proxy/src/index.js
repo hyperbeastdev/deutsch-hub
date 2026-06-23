94c1b5ddb9d5dc0b7ac6b2dfe56deb56d054c5e0
Hyperbeast-ai <saurabhk137070@gmail.com>
2026-06-23 15:24:29 +0530

    Fix proxy URL duplication

--- cloudflare-proxy/src/index.js
+++ cloudflare-proxy/src/index.js
@@ -1,7 +1,7 @@
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "*",
 };
 
 export default {
